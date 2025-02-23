b3a31001a57748f49ebbba9fba21a1fe9914efb9
zmc <qazxcvfds123@qq.com>
2025-02-23 23:33:06 +0800

    mod: 点击文本链接跳转网页

--- common/src/main/java/com/example/common/ui/TextInfoDialog.kt
+++ common/src/main/java/com/example/common/ui/TextInfoDialog.kt
@@ -1,29 +1,37 @@
 package com.example.common.ui
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.DialogProperties
 import com.example.common.R
+import com.example.common.WebActivity.Companion.navWeb
 import com.example.common.util.LocalMainViewModel
 import com.example.common.util.getString
+import java.util.regex.Pattern
 
 @Composable
 fun TextInfoDialog(
     title: String,
-    message: String
+    message: String,
 ) {
     val mainViewModel = LocalMainViewModel.current
-    val scope = rememberCoroutineScope()
     AlertDialog(
         onDismissRequest = { mainViewModel.hideTextInfo() },
         properties = DialogProperties(
@@ -39,12 +47,7 @@ fun TextInfoDialog(
             )
         },
         text = {
-            Text(
-                text = message,
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                lineHeight = 24.sp
-            )
+            LinkText(parseUrl(message))
         },
         confirmButton = {
             OutlinedButton(
@@ -63,4 +66,60 @@ fun TextInfoDialog(
         containerColor = MaterialTheme.colorScheme.surface,
         tonalElevation = 6.dp
     )
+}
+
+@Composable
+fun LinkText(inputText: MutableList<Pair<String, Boolean>>) {
+    val context = LocalContext.current
+    val annotatedString = buildAnnotatedString {
+        inputText.forEach { (text, isUrl) ->
+            if (isUrl) {
+                pushStringAnnotation(tag = "URL", annotation = text)
+                withStyle(
+                    SpanStyle(
+                        color = MaterialTheme.colorScheme.primary,
+                        textDecoration = TextDecoration.Underline
+                    )
+                ) {
+                    append(text)
+                }
+            } else {
+                append(text)
+            }
+        }
+    }
+    ClickableText(
+        text = annotatedString,
+        style = LocalTextStyle.current.copy(
+            fontSize = 16.sp,
+            color = Color.Black
+        ),
+        modifier = Modifier.padding(bottom = 8.dp),
+        onClick = { offset ->
+            val url = annotatedString
+                .getStringAnnotations("URL", offset, offset)
+                .lastOrNull() ?: return@ClickableText
+            context.navWeb(url.item)
+        }
+    )
+}
+
+fun parseUrl(input: String): MutableList<Pair<String, Boolean>> {
+    val pattern = Pattern.compile("http.*?#!")
+    val matcher = pattern.matcher(input)
+    val result = mutableListOf<Pair<String, Boolean>>()
+    var lastEnd = 0
+    while (matcher.find()) {
+        val start = matcher.start()
+        val end = matcher.end()
+        if (start > lastEnd) {
+            result.add(Pair(input.substring(lastEnd, start), false))
+        }
+        result.add(Pair(input.substring(start, end - 2), true))
+        lastEnd = end
+    }
+    if (lastEnd < input.length) {
+        result.add(Pair(input.substring(lastEnd), false))
+    }
+    return result
 }
